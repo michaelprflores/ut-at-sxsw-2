bd04a2911fe9dc3d7ea7710f8d61be3e0d23ceb4
Eric Ramirez <eric.ramirez@austin.utexas.edu>
2023-01-14 14:58:46 -0600

    first commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,7 +6,7 @@ import Seo from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
+    <h1 className="text-2xl">Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
