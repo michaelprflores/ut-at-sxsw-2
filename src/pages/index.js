b43d2717c6dd40269fbba09be587e00ccb2d029b
Eric Ramirez <eric.ramirez@austin.utexas.edu>
2023-01-18 10:50:28 -0600

    add placeholder equals none to all media

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,15 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <div className="block md:hidden">
-      <StaticImage src="../images/discover-texas.png" alt="UT SXSW" />
+      <StaticImage
+        src="../images/discover-texas.png"
+        alt="UT SXSW"
+        placeholder="none"
+      />
     </div>
     <div className="hidden md:block pt-[10%] xl:pt-[5%]">
       <Spline
+        placeholder="none"
         className="md:!h-[80vw] lg:!h-[60vw] xl:!h-[50vw]"
         scene="https://prod.spline.design/eh4TRGCbw1YpdzxE/scene.splinecode"
       />
